b758298932530dc0551e8cedd79465af2934a0f2
Nathan Rozsa <nathan@rozsa.net>
2026-05-21 15:02:07 -0600

    added a photo to about page

--- next-env.d.ts
+++ next-env.d.ts
@@ -1,6 +1,10 @@
 /// <reference types="next" />
 /// <reference types="next/image-types/global" />
+<<<<<<< HEAD
 import "./.next/dev/types/routes.d.ts";
+=======
+/// <reference path="./.next/types/routes.d.ts" />
+>>>>>>> f636e2d2246530b663a4dd1300d6b3017eb46041
 
 // NOTE: This file should not be edited
 // see https://nextjs.org/docs/app/api-reference/config/typescript for more information.
