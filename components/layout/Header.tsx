30dd93be7b8b073d40a54ad5d8fb5ac12210d300
Nathan Rozsa <nathan@rozsa.net>
2026-06-11 14:19:43 -0600

    updated header logo to be bigger

--- components/layout/Header.tsx
+++ components/layout/Header.tsx
@@ -29,7 +29,8 @@ export default function Header() {
               alt="University of Montana Merch Store"
               width={40}
               height={40}
-              className="h-10 w-auto"
+              className="h-14
+               w-auto"
               priority
             />
           </Link>
